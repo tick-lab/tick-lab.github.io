35975573ba233e2d644797a18a631a088d732353
aemann01 <aemann01@ou.edu>
2019-10-30 13:32:19 -0500

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20191030131537';
+const version = '20191030133034';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://github.com/tick-lab/tick-lab.github.io/img/tick_thumbnail.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
